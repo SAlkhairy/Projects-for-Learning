98ee3e1c48098adbfcb3915c2eed26346b1857d8
U-LEVO\Saleha <salkhairy@hotmail.com>
2018-05-28 16:09:27 +0300

    Admin Edits - Last Session; Hooary!

--- newsapp/admin.py
+++ newsapp/admin.py
@@ -6,5 +6,16 @@ from django.contrib import admin
 from .models import Article, Comment
 # Register your models here.
 
-admin.site.register(Article)
+def make_reviewed(ModelAdmin, request, queryset):
+    queryset.update(is_reviewed=True)
+make_reviewed.short_description = "تغيير حالة المراجعة"
+
+class ArticleAdmin(admin.ModelAdmin):
+    list_display =  ['title', 'author', 'pub_date', 'is_reviewed']
+    list_filter = ['author', 'is_reviewed']
+    search_fields = ['title', 'author', 'body']
+    actions = [make_reviewed]
+
+
+admin.site.register(Article, ArticleAdmin)
 admin.site.register(Comment)
--- newsapp/models.py
+++ newsapp/models.py
@@ -10,7 +10,17 @@ class Article(models.Model):
     title = models.CharField(max_length=200, default='First Title')
     body = models.TextField(max_length=500)
     image = models.ImageField(blank=True, null=True)
-    pub_date = models.DateTimeField(auto_now_add=True)
+    is_reviewed = models.BooleanField(verbose_name="هل تمت المراجعة؟",
+                                      default=False)
+    pub_date = models.DateTimeField("تاريخ النشر" ,
+                                    auto_now_add=True)
+
+    class Meta:
+        verbose_name = "مفالة"
+        verbose_name_plural = "مقالات"
+
+    def __unicode__(self):
+        return self.title
 
 class Comment(models.Model):
     article = models.ForeignKey(Article)
